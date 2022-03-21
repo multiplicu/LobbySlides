49f30c5b1adf92b0d8474ceee90a29e493c3b95c
Andy Rose <andyrose@hey.com>
2022-03-21 08:05:32 -0500

    feat: new icon and banner with a WebView for the Main activity

---
+++ app/src/main/java/com/multiplicu/lobbyslides/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.multiplicu.lobbyslides
+
+import android.os.Bundle
+import android.view.WindowManager
+import android.webkit.WebView
+import androidx.fragment.app.FragmentActivity
+
+class MainActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Keep the screen on so the screen saver doesn't start
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        // Load the webpage
+        val webView: WebView = findViewById(R.id.webview)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.loadUrl("https://multiplicu.com/")
+    }
+}
